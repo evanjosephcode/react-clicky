8ad79c076b9dc183a0305b75ecae96e71c3d6b29
evanjosephcode <evanseaman89@gmail.com>
2018-06-13 13:02:58 -0500

    added map method to rendering CharacterCard, and pseudocoded logic for to do's

--- src/App.js
+++ src/App.js
@@ -10,86 +10,64 @@ import characters from "./characters.json";
 // import Counter from "./components/Counter";
 
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
+class App extends Component {
 
-const App = () => (
+  // Setting the initial state of the Counter component
+  state = {
+    userScore: 0,
+    characters: characters    
+  };
+
+
+  handleCorrect = () => {
+    // We always use the setState method to update a component's state
+    this.setState({ userScore: this.state.count + 1 });
+  };
+
+  // shuffleData = () => {
+  //   // We always use the setState method to update a component's state
+  //   this.setState({ count: this.state.count + 1 });
+  // };
+
+
+
+
+  handleClick = (id) => {
+    // randomize them regardless of right or wrong
+    let guessedCorrectly = false;
+    for (var i = 0; i < this.state.characters.length; i++) {
+      if (id === this.state.characters[i].id) {
+        if (this.state.characters[i].clicked === false) {
+          this.state.characters[i].clicked = true;
+          this.handleCorrect();
+        }
+        else if (this.state.characters[i].clicked === true) {
+          // characters: this.shuffleData needs to be in ({})
+          this.setState({ userScore: 0 })
+        }
+      }
+    }
+  }
+  
+render() {
+  return (
     <Wrapper>
       <Title>Videogame Characters List</Title>
-      <CharacterCard
-        // name={characters[0].name}
-        image={characters[0].image}
-        // occupation={characters[0].occupation}
-      />
-      <CharacterCard
-        // name={characters[1].name}
-        image={characters[1].image}
-        // occupation={characters[1].occupation}
-      />
-      <CharacterCard
-        // name={characters[2].name}
-        image={characters[2].image}
-        // occupation={characters[2].occupation}
-      />
-      <CharacterCard
-        // name={characters[3].name}
-        image={characters[3].image}
-        // occupation={characters[3].occupation}
-      />
-      <CharacterCard
-        // name={characters[4].name}
-        image={characters[4].image}
-        // occupation={characters[4].occupation}
-      />
-      <CharacterCard
-        // name={characters[5].name}
-        image={characters[5].image}
-        // occupation={characters[5].occupation}
-      />
-      <CharacterCard
-        // name={characters[6].name}
-        image={characters[6].image}
-        // occupation={characters[6].occupation}
-      />
-      <CharacterCard
-        // name={characters[7].name}
-        image={characters[7].image}
-        // occupation={characters[7].occupation}
-      />
-      <CharacterCard
-        // name={characters[8].name}
-        image={characters[8].image}
-        // occupation={characters[8].occupation}
-      />
-      <CharacterCard
-        // name={characters[9].name}
-        image={characters[9].image}
-        // occupation={characters[9].occupation}
-      />
-      <CharacterCard
-        // name={characters[10].name}
-        image={characters[10].image}
-        // occupation={characters[10].occupation}
-      />
-      <CharacterCard
-        // name={characters[11].name}
-        image={characters[11].image}
-        // occupation={characters[11].occupation}
-      />
-    </Wrapper>
-  );
+
+      { this.state.characters.map(character => (
+            <CharacterCard
+            
+            id={character.id}
+            name={character.name}
+            image={character.image}
+            occupation={character.occupation}
+            clicked={character.clicked}
+            handleClick={character.handleClick}
+            />
+          ))}
+    </Wrapper> 
+        )
+  }
+}
 
 export default App;
--- src/components/CharacterCard/CharacterCard.js
+++ src/components/CharacterCard/CharacterCard.js
@@ -6,18 +6,8 @@ import "./CharacterCard.css";
 const CharacterCard = props => (
   <div className="card">
     <div className="img-container">
-      <img alt={props.name} src={props.image} />
+      <img alt={props.name} src={props.image} onClick={() => props.handleClick(props.id)}/>
     </div>
-    {/* <div className="content">
-      <ul>
-        <li>
-          <strong>Name:</strong> {props.name}
-        </li>
-        <li>
-          <strong>Occupation:</strong> {props.occupation}
-        </li>
-      </ul>
-    </div> */}
   </div>
 );
 
