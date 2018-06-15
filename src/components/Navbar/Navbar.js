5860cbf8552d68440b9398097443abdf34e17277
evanjosephcode <evanseaman89@gmail.com>
2018-06-14 21:09:21 -0500

    changed instructions for game

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ export default class Navbar extends Component {
         <nav className="navbar">
         <div className="row mx-auto container-fluid">
             <div className="nav-item col-6 my-auto text-center">
-              Click a character to begin! 
+              Click each character only one time
             </div>
             <div className="nav-item col-6 my-auto text-center">
               Your score : {this.props.userScore} | High score : {this.props.highScore}
